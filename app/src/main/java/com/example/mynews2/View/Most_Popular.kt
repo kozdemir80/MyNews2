4143fc4ec644cdfb6107c53ffa73657f70b4e6fc
kozdemir80 <k.ozdemir80@gmail.com>
2022-01-26 05:57:18 +0300

    asda

--- app/src/main/java/com/example/mynews2/View/Most_Popular.kt
+++ app/src/main/java/com/example/mynews2/View/Most_Popular.kt
@@ -1,12 +1,12 @@
 package com.example.mynews2.View
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -19,26 +19,37 @@ import com.example.mynews2.R
 import com.example.mynews2.R.*
 import com.example.mynews2.ViewModel.MostPopularViewModel
 import com.example.mynews2.ViewModel.ViewModelFactory
-import okhttp3.internal.notify
 
 
-class Most_Popular:Fragment(R.layout.most_popular) {
+class Most_Popular:Fragment(layout.most_popular) {
 
 
-    val mrecyclerView = view?.findViewById<RecyclerView>(R.id.recyclerData)
+   lateinit var recyclerView: RecyclerView
     private lateinit var viewModel: MostPopularViewModel
-    lateinit var newsAdapter: NewsAdapter
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val rootView= inflater.inflate(layout.most_popular, container, false)
+        recyclerView= rootView?.findViewById(R.id.recyclerData)!!
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager=LinearLayoutManager(activity)
+        recyclerView.adapter=NewsAdapter()
+        return rootView
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setUpRecyclerview()
         val repository= Repository()
         val viewModelFactory= ViewModelFactory(repository)
         viewModel= ViewModelProvider(this,viewModelFactory).get(MostPopularViewModel::class.java)
         viewModel.getMostPopularNews()
-        viewModel.myResponse.observe(viewLifecycleOwner, Observer { reponse->
+        viewModel.myResponse.observe(viewLifecycleOwner, { reponse->
             Log.d("Response",reponse.copyright)
             Log.d("Response",reponse.status)
             Log.d("Response", reponse.num_results.toString())
@@ -47,11 +58,4 @@ class Most_Popular:Fragment(R.layout.most_popular) {
         })
     }
 
-    private fun setUpRecyclerview(){
-        newsAdapter=NewsAdapter()
-        mrecyclerView?.apply {
-            adapter=newsAdapter
-            layoutManager=LinearLayoutManager(activity)
-        }
-    }
 }
